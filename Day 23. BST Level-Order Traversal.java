d867bfa128dde8096daaa08f9cadc9bd072d98cb
INT31302 <tkdwo287@gmail.com>
2020-05-09 20:47:34 +0900

    Day 23

---
+++ Day 23. BST Level-Order Traversal.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+static void levelOrder(Node root){
+      //Write your code here
+      LinkedList<Node> list = new LinkedList<>();
+        list.add(root);
+        while (!list.isEmpty()) {
+            Node tempNode = list.poll();
+            System.out.print(tempNode.data + " ");
+            if (tempNode.left != null) {
+                list.add(tempNode.left);
+            }
+            if (tempNode.right != null) {
+                list.add(tempNode.right);
+            }
+        }
+    }
+
+public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            levelOrder(root);
+        }	
+}
